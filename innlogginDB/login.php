88973836e58e4dae46a45c0d565d71e85bf954b0
Hermann-Skole <hwolden@pm.me>
2025-01-31 11:00:17 +0100

    dasd

--- innlogginDB/login.php
+++ innlogginDB/login.php
@@ -8,11 +8,20 @@
 <body>
     <?php
         session_start();
-        $server = "localhost";
+
+        // lager variabler for all infoen man trenger for databasen
+        $servername = "localhost";
         $database = "innlogging";
         $dbUser = "root";
         $dbPassord = "";
 
+        // prøver å åpne en connection med databsen
+        try{
+            $conn = new PDO("mysql:host=$servername;dbname=$database;charset=utf8", $dbUser, $dbPassord);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e){
+
+        };
     ?>
 </body>
 </html>
